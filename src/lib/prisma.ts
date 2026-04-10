4b8915f25f5a3ccbd593f2d6db81526605a0532c
Marina Kamel <126111065+MarinaKamel-coder@users.noreply.github.com>
2026-04-10 11:41:49 -0400

    fix: modifier le prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+dotenv.config();
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error", "warn"],
-  });
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({
+  adapter,
+  log: ['query', 'info', 'warn', 'error'],
+});
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+export default prisma;
